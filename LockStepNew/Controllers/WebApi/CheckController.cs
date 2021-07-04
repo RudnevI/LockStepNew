124551722dcc55e06641afa39fd46f981063c55e
me <me>
2021-07-04 22:34:41 +0600

    check web-api controller and pay web api controller

---
+++ LockStepNew/Controllers/WebApi/CheckController.cs
@@ -0,0 +1,69 @@
+﻿using LockStep.Library.Domain;
+using LockStep.Library.Domain.Finance;
+using LockStepNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace LockStepNew.Controllers.WebApi
+{
+    public class CheckController : ApiController
+    {
+        // GET: Check
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+        // GET: Default
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            try
+            {
+
+                if (!IsValidId(id)) return BadRequest();
+                Book book = await GetBook(id);
+                if (book == null) return NotFound();
+                List<Price> prices = _context.Prices.Where(p => p.Book.Id == id).ToList();
+
+                if (prices == null || prices.Count == 0) return NotFound();
+                Price price = prices.Where(p => p.To == prices.Select(s => s.From).ToList().Max()).FirstOrDefault();
+                if (price == null) price = prices.Where(p => p.From == null).FirstOrDefault();
+
+                return Ok(new { book.Name, price.Value });
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+
+
+        }
+
+        private int? GetMaxId()
+        {
+            return _context.Books.Select(b => b.Id).Max();
+        }
+
+        private bool IsValidId(int id)
+        {
+            int? maxId = GetMaxId();
+            //TODO: Рассмотреть вариант с maxId равным null
+            return !(id <= 0 || id > maxId);
+        }
+
+
+
+        private async Task<Book> GetBook(int id)
+        {
+            try
+            {
+                return await _context.Books.FindAsync(id);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ LockStepNew/Controllers/WebApi/PayController.cs
@@ -0,0 +1,88 @@
+﻿using LockStep.Library.Domain;
+using LockStep.Library.Domain.Finance;
+using LockStepNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace LockStepNew.Controllers.WebApi
+{
+    public class PayController : ApiController
+    {
+
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+      
+
+    
+
+        public async Task<IHttpActionResult> Get(int id, double sum, string email)
+        {
+            if (!IsValidId(id) || !(await BookExists(id))) return NotFound();
+            if (!IsValidsum(sum)) return BadRequest();
+
+            Book book = await GetBook(id);
+            Check check = new Check() {Book=book, Email=email};
+            Payment payment = new Payment() { Book = book, Email = email, Amount = (decimal)sum };
+
+            AddCheck(check);
+            AddPayment(payment);
+
+            return Ok();
+
+
+
+        }
+
+
+        private void AddPayment(Payment payment)
+        {
+            _context.Payments.Add(payment);
+            _context.SaveChanges();
+        }
+
+        private void AddCheck(Check check)
+        {
+             _context.Checks.Add(check);
+            _context.SaveChanges();
+        }
+
+        private async Task<bool> BookExists(int id)
+        {
+            return await GetBook(id) != null;
+        }
+
+        private async Task<Book> GetBook(int id)
+        {
+            try
+            {
+                return await _context.Books.FindAsync(id);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private int? GetMaxId()
+        {
+            return _context.Books.Select(b => b.Id).Max();
+        }
+
+        private bool IsValidId(int id)
+        {
+            int? maxId = GetMaxId();
+            //TODO: Рассмотреть вариант с maxId равным null
+            return !(id <= 0 || id > maxId);
+        }
+
+       private bool IsValidsum(double sum)
+        {
+
+            return sum >= 0;
+        }
+    }
+}
